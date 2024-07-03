f3a9337691a37ec10c864507af0188613a3e3b41
rinnegard <victor.rinnegard@hotmail.com>
2024-07-03 14:35:04 +0200

    Fixed details missing completed value

--- app/todo/[todoId]/page.tsx
+++ app/todo/[todoId]/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/app/components/header";
 import { getTodo } from "../actions";
+import { revalidatePath } from "next/cache";
 
 export type TodoDetailsPageProps = {
     params: {
@@ -9,15 +10,16 @@ export type TodoDetailsPageProps = {
 
 export default async function TodoDetails({ params }: TodoDetailsPageProps) {
     const todo = await getTodo(params.todoId);
+    revalidatePath("/todo/" + params.todoId);
 
     return (
         <div>
             <Header title="Todo Details"></Header>
             <div>ID: {todo?.id}</div>
             <div>Desc: {todo?.value}</div>
-            <div>Completed: {todo?.completed}</div>
-            <div>Created: {todo?.createdAt.toUTCString()}</div>
-            <div>Updated: {todo?.updatedAt?.toUTCString()}</div>
+            <div>Completed: {todo?.completed ? "Yes" : "No"}</div>
+            <div>Created: {todo?.createdAt.toLocaleString()}</div>
+            <div>Updated: {todo?.updatedAt?.toLocaleString()}</div>
         </div>
     );
 }
